11192c8ff54eeb775732f707c2f8be67b8e4e577
Puneet <puneet2715@gmail.com>
2021-06-29 09:14:43 +0530

    implementing public chat feature

--- app.js
+++ app.js
@@ -8,28 +8,28 @@ const app = express()
 
 let sessionOptions = session({
     secret: "superSecret",
-    store: new MongoStore({client: require('./db')}),
+    store: new MongoStore({ client: require('./db') }),
     resave: false,
     saveUninitialized: false,
-    cookie: {maxAge: 1000 * 60 * 60 * 24, httpOnly: true}
+    cookie: { maxAge: 1000 * 60 * 60 * 24, httpOnly: true }
 })
 
 app.use(sessionOptions)
 app.use(flash())
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
 
     // make our markdown function available from within ejs template
-    res.locals.filterUserHTML = function(content) {
-        return sanitizeHTML(markdown(content), {allowedTags: ['p', 'br', 'ul', 'ol', 'bold', 'i', 'h1'], allowedAttributes: {}})
+    res.locals.filterUserHTML = function (content) {
+        return sanitizeHTML(markdown(content), { allowedTags: ['p', 'br', 'ul', 'ol', 'bold', 'i', 'h1'], allowedAttributes: {} })
     }
 
     // make all error and success flash messages available from all templates
     res.locals.errors = req.flash("errors")
     res.locals.success = req.flash("success")
-    
+
     //make current user id avilable on the req object
-    if(req.session.user) {req.visitorId = req.session.user._id} else {req.visitorId = 0}
+    if (req.session.user) { req.visitorId = req.session.user._id } else { req.visitorId = 0 }
 
     //make user session data available from within view templates
     res.locals.user = req.session.user
@@ -38,7 +38,7 @@ app.use(function(req, res, next) {
 
 const router = require('./router')
 
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 app.use(express.static('public'))
@@ -47,4 +47,25 @@ app.set('view engine', 'ejs')
 
 app.use('/', router)
 
-module.exports = app
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+
+io.use(function (socket, next) {
+    sessionOptions(socket.request, socket.request.res, next)
+})
+
+io.on('connection', function (socket) {
+    if (socket.request.session.user) {
+        let user = socket.request.session.user
+
+        socket.emit('welcome', { username: user.username, avatar: user.avatar })
+
+        socket.on('chatMessageFromBrowser', function (data) {
+            socket.broadcast.emit('chatMessageFromServer', { message: sanitizeHTML(data.message, { allowedTags: [], allowedAttributes: {} }), username: user.username, avatar: user.avatar }) //sending message to everyone except the author of the message
+            //socket.emit() -> this should be used if we wanted to emit the message only to the browser that sent the message
+            //io.emit() -> used io with the emit function to broadcast the message to every connected user
+        })
+    }
+})
+
+module.exports = server
